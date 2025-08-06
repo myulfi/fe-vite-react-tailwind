8c4d01f4ed1fa1a81d0228e405ac96033656a802
Mulyadi Yulfi <myulfi@xl.co.id>
2025-08-06 15:31:30 +0700

    Add getMenu

--- src/components/containers/SideBar.tsx
+++ src/components/containers/SideBar.tsx
@@ -1,69 +1,28 @@
 import { useEffect, useState } from "react"
 import { Link, useLocation } from "react-router-dom"
-import { SIDEBAR_WIDTH } from "../../constants/common-constants"
+import { LOCAL_STORAGE, SIDEBAR_WIDTH } from "../../constants/common-constants"
+import { apiRequest } from "../../api"
+import { useTranslation } from "react-i18next"
 
 type MenuItem = {
-    label: string
+    name: string
     icon?: React.ReactNode
-    link?: string
+    path?: string
     children?: MenuItem[]
 }
-// const menuData: MenuItem[] = [
-//     {
-//         label: 'Home',
-//         icon: "fa-solid fa-house",
-//         link: '/',
-//     },
-//     {
-//         label: 'Pengusaha',
-//         icon: "fa-solid fa-user-tie",
-//         link: '/businessman',
-//     },
-// ]
-const menuData: MenuItem[] = [
-    {
-        label: 'Home',
-        icon: 'fa-solid fa-house',
-        link: '/',
-    },
-    {
-        label: 'Analytics',
-        icon: 'fa-solid fa-house',
-        children: [
-            {
-                label: 'Reports',
-                children: [
-                    {
-                        label: 'Daily',
-                        children: [
-                            { label: 'Morning', link: '/analytics/reports/daily/morning' },
-                            { label: 'Afternoon', link: '/analytics/reports/daily/afternoon' },
-                        ],
-                    },
-                    { label: 'Monthly', link: '/analytics/reports/monthly' },
-                ],
-            },
-            { label: 'Insights', link: '/insights' },
-        ],
-    },
-    {
-        label: 'Example Template',
-        icon: 'fa-solid fa-house',
-        link: '/test/example-template.html',
-    },
-]
 
 const SidebarItem: React.FC<{ item: MenuItem; level?: number; onNavigate?: () => void }> = ({ item, level = 0, onNavigate }) => {
+    const { t } = useTranslation();
     const location = useLocation()
     const [openFlag, setOpenFlag] = useState(false)
     const hasChildrenFlag = !!item.children?.length
 
-    const activeFlag = item.link === location.pathname
+    const activeFlag = item.path === location.pathname
 
     const hasActiveChildFlag = (children?: MenuItem[]): boolean => {
         if (!children) return false
         return children.some(child => {
-            if (child.link && location.pathname === child.link) return true
+            if (child.path && location.pathname === child.path) return true
             return hasActiveChildFlag(child.children)
         })
     }
@@ -85,14 +44,14 @@ const SidebarItem: React.FC<{ item: MenuItem; level?: number; onNavigate?: () =>
                     <span className='absolute -left-5 top-1/2 -translate-y-1/2 w-2 h-2 bg-light-clear dark:bg-dark-clear rounded-full'></span>
                 )}
 
-                {item.link && !hasChildrenFlag ? (
+                {item.path && !hasChildrenFlag ? (
                     <Link
-                        to={item.link}
+                        to={item.path}
                         onClick={onNavigate}
                         className={`flex items-center w-full p-2 hover:text-light-base-line hover:dark:hover:text-dark-base-line hover:bg-light-clear-secondary hover:dark:bg-dark-clear-secondary cursor-pointer rounded-l-lg ${activeFlag ? 'text-light-base-line dark:text-dark-base-line bg-light-clear-secondary dark:bg-dark-clear-secondary font-semibold' : ''}`}
                     >
                         {item.icon && <span className='mr-2'><i className={`${item.icon}`} /></span>}
-                        {item.label}
+                        {t(item.name)}
                     </Link>
                 ) : (
                     <button
@@ -100,7 +59,7 @@ const SidebarItem: React.FC<{ item: MenuItem; level?: number; onNavigate?: () =>
                         className="flex items-center w-full p-2 pl-2 hover:text-light-base-line hover:dark:hover:text-dark-base-line hover:bg-light-clear-secondary hover:dark:bg-dark-clear-secondary cursor-pointer rounded-l-lg"
                     >
                         {item.icon && <span className="mr-2"><i className={`${item.icon}`} /></span>}
-                        {item.label}
+                        {t(item.name)}
                         {hasChildrenFlag && <span className="ml-auto pr-4"><i className={`fa-solid text-xs ${openFlag ? 'fa-angle-down' : 'fa-angle-right'}`} /></span>}
                     </button>
                 )}
@@ -132,6 +91,24 @@ export default function SideBar({
     sidebarOpenFlag,
     setSidebarOpenFlag,
 }: SideBarProps) {
+    const [menuList, setMenuList] = useState([]);
+
+    useEffect(() => {
+        // i18n.changeLanguage(Cookies.get(CommonConstants.COOKIES.LANGUAGE) ?? "en")
+        const fetchMenu = async () => {
+            try {
+                if (localStorage.getItem(LOCAL_STORAGE.ACCESS_TOKEN) !== null) {
+                    const response = await apiRequest("get", "/main/menu.json");
+                    setMenuList(response.data);
+                }
+            } catch (error) {
+                console.error("Failed to fetch menu:", error);
+            }
+        };
+
+        fetchMenu();
+    }, []);
+
     return (
         <div
             className={`fixed top-0 left-0 h-full overflow-y-auto bg-light-clear dark:bg-dark-clear text-light-base-line dark:text-dark-base-line z-20 transition-transform duration-500 ease-in-out sm:w-[256px] max-sm:w-screen ${sidebarOpenFlag ? 'translate-x-0' : '-translate-x-full'}`}
@@ -148,7 +125,8 @@ export default function SideBar({
             </div>
 
             <nav className='pl-4'>
-                {menuData.map((item, idx) => (
+
+                {menuList.map((item, idx) => (
                     <SidebarItem
                         key={idx}
                         item={item}
